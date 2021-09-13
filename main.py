3648bb9f47d0bbb43c5fc92578eb57d34f10ee77
Marco Georgaklis <marco.georgaklis@vanderbilt.edu>
2021-09-12 21:39:45 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+utilities = [
+    "find",
+    "xargs",
+    "grep",
+    "rm",
+    "echo",
+    "ls",
+    "sort",
+    "chmod",
+    "wc",
+    "cat",
+    "cut",
+    "head",
+    "mv",
+    "chown",
+    "cp",
+    "mkdir",
+    "tr",
+    "tail",
+    "dirname",
+    "rsync"
+]
